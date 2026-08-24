5928f824a68c23566263cae707206b3a0dceb609
Aryan sahu <lakhan9953@gmail.com>
2026-08-24 21:37:46 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class loop {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello World");
+        for(int counter= 0 ; counter<11; counter=counter +1 ){
+            System.out.println(counter);
+        }
+    }
+}
