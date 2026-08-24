4184f02530fc7c690764c3576c3f2e5cb783e36b
Aryan sahu <lakhan9953@gmail.com>
2026-08-24 22:09:52 +0530

    Correct increment syntax in loop3.java
    Fixed increment operator in the do-while loop.

--- loop3.java
+++ loop3.java
@@ -8,7 +8,7 @@ public class loop3 {
         int i = 1;
         do {
             System.out.println(a+"X"+ i + "=" + (a*i));
-            i++;
+            i= i+1;
         }while(i<11);
     
     
