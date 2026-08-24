e29d46f3d8d1aa3ee8104eead53ee4851dfb284a
Aryan sahu <lakhan9953@gmail.com>
2026-08-24 22:06:06 +0530

    Add files via upload

---
+++ loop3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+//do while loop implimentation
+public class loop3 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of which you want the Table");
+        int a = sc.nextInt();
+        int i = 1;
+        do {
+            System.out.println(a+"X"+ i + "=" + (a*i));
+            i++;
+        }while(i<11);
+    
+    
+    
+    }
+}
