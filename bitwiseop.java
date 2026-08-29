839e1bd848cac145f84e5fdaab7a310e1c3f43e2
Aryan sahu <lakhan9953@gmail.com>
2026-08-29 11:20:15 +0530

    adding XOR andNOT

--- bitwiseop.java
+++ bitwiseop.java
@@ -1,9 +1,13 @@
 public class bitwiseop {
     /*Logical Operator are AND,Bitwise OR XOR NOT << >> . 
-    these basically works like the logic Gates which we learn in class */
+    these basically works like the logic Gates which we learn in class
+    so all numbers are Coverted into Bits */
     public static void main(String[] arg){
         System.out.println(4&5); // here 4--> 100 and 5-->101 and there AND is 100
         System.out.println(4|5); // here 4--> 100 and 5-->101 and there OR is 101
+        System.out.println(4^5); // there XOR is 1 which 001 in Binary
+        System.out.println(~5); // this is NOT and its Output will be -(n+1)
+
     }
     
 
