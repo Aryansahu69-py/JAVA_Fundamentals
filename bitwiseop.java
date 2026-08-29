7ab296640d2cd8af0a4e6414ce9f31893057e650
Aryan sahu <lakhan9953@gmail.com>
2026-08-29 11:10:23 +0530

    adding Bitwise OP

---
+++ bitwiseop.java
@@ -0,0 +1,10 @@
+public class bitwiseop {
+    /*Logical Operator are AND,Bitwise OR XOR NOT << >> . 
+    these basically works like the logic Gates which we learn in class */
+    public static void main(String[] arg){
+        System.out.println(4&5); // here 4--> 100 and 5-->101 and there AND is 100
+        System.out.println(4|5); // here 4--> 100 and 5-->101 and there OR is 101
+    }
+    
+
+}
