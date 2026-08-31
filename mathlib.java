5fc330532a06a8b68d1058cc8971a19d327ecac8
Aryan sahu <lakhan9953@gmail.com>
2026-08-31 21:52:02 +0530

    Adding some Functions

--- mathlib.java
+++ mathlib.java
@@ -5,6 +5,9 @@ This class Comes under lang package which is by default imported in every progra
 
 public class mathlib {
     public static void main(String args[]){
+        System.out.println(Math.max(12,23 ));
+        System.out.println(Math.min(12, 3));
+        System.out.println(Math.abs(-6)); //it gives the Modulus of the Number
         
     }
     
