2cf567f04076373b23367d9340ee8b599b9aff92
Aryan sahu <lakhan9953@gmail.com>
2026-08-31 21:57:55 +0530

    Adding some More functions

--- mathlib.java
+++ mathlib.java
@@ -8,6 +8,10 @@ public class mathlib {
         System.out.println(Math.max(12,23 ));
         System.out.println(Math.min(12, 3));
         System.out.println(Math.abs(-6)); //it gives the Modulus of the Number
+        System.out.println(Math.floor(2.45));//Gives the Lower Value in float
+        System.out.println(Math.ceil(2.45));//Gives the upper value in float
+        System.out.println(2.45);//Gives the Round value in Integer
+        System.out.println(Math.pow(2, 3)); // give the a raise to power b 
         
     }
     
