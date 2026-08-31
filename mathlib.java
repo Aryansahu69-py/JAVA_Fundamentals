9b5217a0718b0ab912c002a007af5bd9fd06d412
Aryan sahu <lakhan9953@gmail.com>
2026-08-31 21:40:22 +0530

    Learing Math Class in JAVA

---
+++ mathlib.java
@@ -0,0 +1,11 @@
+/*
+Topic -: Math class 
+This class Comes under lang package which is by default imported in every programe  
+*/
+
+public class mathlib {
+    public static void main(String args[]){
+        
+    }
+    
+}
