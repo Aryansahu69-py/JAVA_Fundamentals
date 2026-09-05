63beaf9d06e2602d06e4224e5c7c18523fe32bf2
Aryan sahu <lakhan9953@gmail.com>
2026-09-05 23:19:39 +0530

    One small Code

---
+++ prac2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class prac2 {
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First Number");
+        int a = sc.nextInt();
+        System.out.println("Enter the Second Number");
+        int b = sc.nextInt();
+        if(a>b)
+            System.out.println(a + "is greater than" + b);
+        else
+            System.out.println(b + "is greater than " + a);
+    }
+}
